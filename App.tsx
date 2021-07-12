5ec87909d39650efee5f6f087eca9b3488df90a3
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-12 20:53:27 -0300

    Config fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { Signin } from "./src/pages/Signin";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+  //hold a home screen
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Signin />;
 }
