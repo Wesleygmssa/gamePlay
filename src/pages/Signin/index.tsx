1a690c0718be9716a2c457815c4465dc30034018
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-11 21:24:10 -0300

    Create page signin

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,14 +1,27 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, Image, StatusBar } from "react-native";
 import { styles } from "./styles";
+import Illustration from "../../assets/illustration.png";
 
 export function Signin() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-      <TextInput style={styles.input} />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image source={Illustration} style={styles.image} resizeMode="stretch" />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}suas jogatinhas{`\n`}facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+      </View>
     </View>
   );
 }
