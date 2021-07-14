33ad3e358da26449f7ad2d955b1d395e18867134
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-14 20:16:35 -0300

    Adjusting button

--- src/components/Buttonicon/index.tsx
+++ src/components/Buttonicon/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import {
   Text,
   View,
@@ -9,18 +10,18 @@ import {
 import { styles } from "./styles";
 import DiscordImg from "../../assets/discord.png";
 
-type Props = TouchableOpacityProps & {
+type Props = RectButtonProps & {
   title: string;
 };
 
 export function ButtonIcon({ title, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <RectButton style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
 
       <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
