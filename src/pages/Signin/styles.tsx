6f758eb1636649f409b9f25523044fbe5cc27aed
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-15 19:05:25 -0300

    Adjust button on screen

--- src/pages/Signin/styles.tsx
+++ src/pages/Signin/styles.tsx
@@ -9,7 +9,7 @@ export const styles = StyleSheet.create({
   },
   image: {
     width: "100%",
-    height: 360,
+    height: 340,
   },
   content: {
     marginTop: -40,
@@ -27,7 +27,7 @@ export const styles = StyleSheet.create({
     color: theme.colors.heading,
     fontSize: 15,
     textAlign: "center",
-    marginBottom: 64,
+    marginBottom: 45,
     fontFamily: theme.fonts.title500,
     lineHeight: 25,
   },
