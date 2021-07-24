feca8a13dd3476c9ae72615345fda78705c1abef
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-24 19:05:54 -0300

    Building home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
 import { Profile } from "../../components/Profile";
 import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
 import { styles } from "./styles";
 export function Home() {
   const [category, setCategory] = useState("");
@@ -12,6 +14,19 @@ export function Home() {
   const appointments = [
     {
       id: "1",
+      guild: {
+        id: "1",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "22/06 às 10:40h",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+    {
+      id: "2",
       guild: {
         id: "1",
         name: "Lendários",
@@ -47,7 +62,10 @@ export function Home() {
         <FlatList
           data={appointments}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <Text>{item.guild.name}</Text>}
+          renderItem={({ item }) => <Appointment data={item} />}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
         />
       </View>
     </View>
