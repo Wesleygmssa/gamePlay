640393248e30d55595dbf844bbc79a8a12ceedcd
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-03 21:28:29 -0300

    Creating game home page

--- src/pages/AppointmentCreate/index.tsx
+++ src/pages/AppointmentCreate/index.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
 
 import { Header } from "../../components/Header";
+import { theme } from "../../global/styles/theme";
+import { CategorySelect } from "../../components/CategorySelect";
 
 import { Background } from "../../components/Background";
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+import { RectButton } from "react-native-gesture-handler";
 
 export function AppointmentCreate() {
+  const [category, setCategory] = useState("");
   return (
     <Background>
       <Header title="Agendar partida" />
+      <Text style={styles.label}>Categoria</Text>
+
+      <CategorySelect
+        hasCheckBox
+        setCategory={setCategory}
+        categorySelected={category}
+      />
+
+      <View style={styles.form}>
+        <RectButton>
+          <View style={styles.select}>
+            <View style={styles.image} />
+            <View style={styles.selectBody}>
+              <Text style={styles.label}>Selecione um servidor</Text>
+            </View>
+            <Feather name="chevron-right" color={theme.colors.heading} />
+          </View>
+        </RectButton>
+      </View>
     </Background>
   );
 }
--- src/pages/AppointmentCreate/styles.ts
+++ src/pages/AppointmentCreate/styles.ts
@@ -1,3 +1,4 @@
+import { Inter_100Thin } from "@expo-google-fonts/inter";
 import { StyleSheet } from "react-native";
 import {
   getBottomSpace,
@@ -8,5 +9,36 @@ import { theme } from "../../global/styles/theme";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  label: {
+    fontSize: 18,
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading,
+  },
+  form: {
+    paddingHorizontal: 24,
+    marginTop: 32,
+  },
+  select: {
+    flexDirection: "row",
+    width: "100%",
+    height: 68,
+    borderColor: theme.colors.secondary50,
+    borderWidth: 1,
+    borderRadius: 8,
+    alignItems: "center",
+    paddingRight: 25,
+    overflow: "hidden",
+  },
+  selectBody: {
+    flex: 1,
+    alignItems: "center",
+  },
+  image: {
+    width: 64,
+    height: 68,
+    backgroundColor: theme.colors.secondary50,
+    borderWidth: 1,
+    borderRadius: 8,
   },
 });
