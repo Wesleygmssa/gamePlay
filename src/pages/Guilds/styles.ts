fa8708464861b84a6d39e3955dd60f45ad2db2a0
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-19 21:55:20 -0300

    Ajuste estilos da guilds

---
+++ src/pages/Guilds/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from "react-native-iphone-x-helper";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+});
