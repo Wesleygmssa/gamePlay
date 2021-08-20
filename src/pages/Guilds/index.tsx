22c1263511de631b0bbf297848c1c911ec7608b5
wesley guerra <wesleyguerra9@gmail.com>
2021-08-20 15:20:20 -0300

    Finalizando modal

--- src/pages/Guilds/index.tsx
+++ src/pages/Guilds/index.tsx
@@ -22,7 +22,7 @@ export function Guilds({ handleGuildSelect }: Props) {
     {
       id: "1",
       name: "Lendários",
-      icon: "image.png",
+      icon: null,
       owner: true,
     },
   ];
