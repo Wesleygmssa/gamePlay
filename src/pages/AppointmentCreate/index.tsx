c743645441b696b720f4f669441c1e2a05a7626b
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-21 15:23:52 -0300

    Ajuste de fechar o modal quando clilcar em qualquer lugar

--- src/pages/AppointmentCreate/index.tsx
+++ src/pages/AppointmentCreate/index.tsx
@@ -32,6 +32,10 @@ export function AppointmentCreate() {
     setOpenGuildsModal(true);
   }
 
+  function handleCloseGuilds() {
+    setOpenGuildsModal(false);
+  }
+
   function handleGuildSelect(guildSelect: GuildProps) {
     setGuild(guildSelect);
     setOpenGuildsModal(false);
@@ -120,7 +124,7 @@ export function AppointmentCreate() {
         </View>
       </ScrollView>
 
-      <ModalView visible={openGuildsModa}>
+      <ModalView visible={openGuildsModa} closeModal={handleCloseGuilds}>
         <Guilds handleGuildSelect={handleGuildSelect} />
       </ModalView>
     </KeyboardAvoidingView>
