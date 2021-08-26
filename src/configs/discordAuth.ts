f897e72bee488ddac82f5c3a67a6319fa7639350
wesley guerra <wesleyguerra9@gmail.com>
2021-08-26 10:39:16 -0300

    Criando variável de configuração

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,7 @@
+const REDIRECT_URI = "https%3A%2F%2Fauth.expo.io%2Fgameplay";
+const SCOPE = "identify%20connections%20email%20guilds";
+const RESPONSE_TYPE = "token";
+const CLIENT_ID = "880439018783924264";
+const CDN_IMAGE = "https://cdn.discordapp.com";
+
+https://discord.com/api
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, ReactNode } from "react";
+import * as AuthSession from "expo-auth-session";
 
 type User = {
   id: string;
@@ -23,7 +24,18 @@ const AuthContext = createContext({} as AuthContextData);
 // Criando função para pegar elementos que ficam por dento do componente
 // Para poder compartilhar a informações
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<User>({});
+  const [user, setUser] = useState<User>({} as User);
+  const [loading, setLoading] = useState(false);
+
+  //Necessário criar um try catch como estamos lidando com contexto externo
+  function SignIn() {
+    try {
+      setLoading(true);
+      const authUrl =
+        "https://discord.com/api/oauth2/authorize?client_id=880439018783924264&redirect_uri=https%3A%2F%2Fauth.expo.io%2Fgameplay%2F&response_type=code&scope=identify%20connections%20email%20guilds";
+      AuthSession.startAsync({ authUrl });
+    } catch (error) {}
+  }
   return (
     /* Estado atual do contexto */
     <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
