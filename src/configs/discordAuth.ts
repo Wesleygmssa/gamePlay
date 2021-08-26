bae13c675a0e9a9a926a7b97e3fcc2e0e60f3cf4
wesley guerra <wesleyguerra9@gmail.com>
2021-08-26 10:43:56 -0300

    Criando configuração axios

--- src/configs/discordAuth.ts
+++ src/configs/discordAuth.ts
@@ -3,5 +3,3 @@ const SCOPE = "identify%20connections%20email%20guilds";
 const RESPONSE_TYPE = "token";
 const CLIENT_ID = "880439018783924264";
 const CDN_IMAGE = "https://cdn.discordapp.com";
-
-https://discord.com/api
