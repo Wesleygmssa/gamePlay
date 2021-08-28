9a6a7be3c6464d8ee529e9420505f6b3a8df21e0
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-28 15:31:01 -0300

    Criando autenticação com discord

--- src/configs/discordAuth.ts
+++ src/configs/discordAuth.ts
@@ -1,4 +1,5 @@
-const REDIRECT_URI = "https%3A%2F%2Fauth.expo.io%2Fgameplay";
+const REDIRECT_URI =
+  "https://auth.expo.io/@anonymous/gameplay-c4f38918-16d3-484c-8227-24a700f4d14b";
 const SCOPE = "identify%20connections%20email%20guilds";
 const RESPONSE_TYPE = "token";
 const CLIENT_ID = "880439018783924264";
