22bd4f1f903d42106c21a53cdc259349cdfd2312
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-28 16:40:26 -0300

    Configurando  informações que serão exibidas do usuário

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Text, TextInput, View, Image, Alert } from "react-native";
+import {
+  Text,
+  TextInput,
+  View,
+  Image,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
 import { styles } from "./styles";
 import Illustration from "../../assets/illustration.png";
 import { ButtonIcon } from "../../components/Buttonicon";
 import { useNavigation } from "@react-navigation/core";
 import { Background } from "../../components/Background";
 import { useAuth } from "../../hooks/auth";
+import { theme } from "../../global/styles/theme";
 
 /* Recendo a informação global */
 
 export function Signin() {
   // const navigation = useNavigation();
-  const { user, signIn } = useAuth();
+  const { signIn, loading } = useAuth();
   async function handleSignIn() {
     // navigation.navigate("Home");
     try {
@@ -37,12 +45,15 @@ export function Signin() {
             Crie grupos para jogar seus games {`\n`}
             favoritos com seus amigos
           </Text>
-
-          <ButtonIcon
-            title="Entrar com Discord"
-            activeOpacity={0.7}
-            onPress={handleSignIn}
-          />
+          {loading ? (
+            <ActivityIndicator color={theme.colors.primary} />
+          ) : (
+            <ButtonIcon
+              title="Entrar com Discord"
+              activeOpacity={0.7}
+              onPress={handleSignIn}
+            />
+          )}
         </View>
       </View>
     </Background>
