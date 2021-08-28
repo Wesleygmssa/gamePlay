98991a6487674bc2c4ffe9ce24a90f05613248d9
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-28 16:40:03 -0300

    Configurando as informações que serão exibidas do usuário

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -20,11 +20,18 @@ type User = {
 
 type AuthContextData = {
   user: User;
+  loading: boolean;
   signIn: () => Promise<void>;
 };
 
 type AuthProviderProps = {
   children: ReactNode;
+};
+
+type AuthorizationResponse = AuthSession.AuthSessionResult & {
+  params: {
+    access_token: string;
+  };
 };
 //Criando contexto, estado inicial do contexto
 // Passando a tipagem com as informações AuthContextData
@@ -42,10 +49,28 @@ export function AuthProvider({ children }: AuthProviderProps) {
     try {
       setLoading(true);
       const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
-      console.log(authUrl);
 
-      const response = await AuthSession.startAsync({ authUrl });
-      console.log(response);
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
+
+      if (type === "success") {
+        api.defaults.headers.authorization = `Bearer ${params.access_token}`;
+
+        const userInfo = await api.get("/users/@me");
+        //EX: ['Wesley', 'Guerra']
+        const firstName = userInfo.data.username.split(" ")[0];
+        //Pegando a imagem de perfil do git Hub
+        userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
+        setUser({
+          ...userInfo.data,
+          firstName,
+          token: params.access_token,
+        });
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
     } catch {
       //Gerando um erro padrão
       // gerando um erro pra quem chamou
@@ -54,7 +79,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
   return (
     /* Estado atual do contexto */
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ user, signIn, loading }}>
       {children}
     </AuthContext.Provider>
   );
