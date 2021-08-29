94f8e1e6f14a67db4fe543476beb60e98f619801
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-28 21:57:01 -0300

    Exibindo as informações do usuário

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "./styles";
 import { Avatar } from "../Avatar";
+import { useAuth } from "../../hooks/auth";
 
 export function Profile() {
+  const { user } = useAuth();
   return (
     <View style={styles.container}>
-      <Avatar urlImage="https://gitHub.com/wesleygmssa.png" />
+      <Avatar urlImage={user.avatar} />
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá</Text>
-          <Text style={styles.username}>Wesley</Text>
+          <Text style={styles.username}>{user.username}</Text>
         </View>
         <Text style={styles.message}>Hoje é dia de vitória</Text>
       </View>
