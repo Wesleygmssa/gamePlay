e14ce32efe0c0dc0bbabe333197687c0af3bd26c
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-03 18:04:57 -0300

    Ajustes finais do app

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useState } from "react";
-import { View, FlatList, Text } from "react-native";
+import { View, FlatList } from "react-native";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
 import { Profile } from "../../components/Profile";
@@ -10,10 +10,16 @@ import { ListDivider } from "../../components/ListDivider";
 import { Background } from "../../components/Background";
 
 import { styles } from "./styles";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { COLLECTION_APPOINTMENTS } from "../../configs/database";
+import { Inter_500Medium } from "@expo-google-fonts/inter";
+import { Load } from "../../components/Load";
 
 export function Home() {
   const [category, setCategory] = useState("");
+  const [loading, setLoadig] = useState(true);
+  const [appointments, setAppointments] = useState<Appointment[]>([]);
 
   const navigation = useNavigation();
 
@@ -25,34 +31,24 @@ export function Home() {
     navigation.navigate("AppointmentCreate");
   }
 
-  const appointments = [
-    {
-      id: "1",
-      guild: {
-        id: "1",
-        name: "Lendários",
-        icon: null,
-        owner: true,
-      },
-      category: "1",
-      date: "22/06 às 10:40h",
-      description:
-        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
-    },
-    {
-      id: "2",
-      guild: {
-        id: "1",
-        name: "Lendários",
-        icon: null,
-        owner: true,
-      },
-      category: "1",
-      date: "22/06 às 10:40h",
-      description:
-        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
-    },
-  ];
+  async function loadAppointments() {
+    const response = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+    const storage: Appointment[] = response ? JSON.parse(response) : [];
+
+    if (category) {
+      setAppointments(storage.filter((item) => item.category === category));
+    } else {
+      setAppointments(storage);
+    }
+
+    setLoadig(false);
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      loadAppointments();
+    }, [category])
+  );
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory("") : setCategory(categoryId);
@@ -70,21 +66,26 @@ export function Home() {
         categorySelected={category}
         setCategory={handleCategorySelect}
       />
-
-      <View style={styles.content}>
-        <ListHeader title="Partidas agendadas" subtitle="Total 6" />
-      </View>
-      <FlatList
-        data={appointments}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Appointment data={item} onPress={handleppointmentDetails} />
-        )}
-        ItemSeparatorComponent={() => <ListDivider />}
-        contentContainerStyle={{ paddingBottom: 69 }}
-        style={styles.matches}
-        showsVerticalScrollIndicator={false}
-      />
+      {loading ? (
+        <Load />
+      ) : (
+        <>
+          <View style={styles.content}>
+            <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+          </View>
+          <FlatList
+            data={appointments}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <Appointment data={item} onPress={handleppointmentDetails} />
+            )}
+            ItemSeparatorComponent={() => <ListDivider />}
+            contentContainerStyle={{ paddingBottom: 69 }}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
+          />
+        </>
+      )}
     </Background>
   );
 }
